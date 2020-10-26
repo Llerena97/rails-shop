5eee7ccc1727f0839592a4c8e34523651ee571e9
Felipe Jose Llerena Catalan <felipe@appmeup.co>
2020-10-25 19:27:36 -0500

    add cart and initial settings

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
-  mount_uploader :image, ImageUploader
+  before_destroy :not_referenced_by_any_line_item
   belongs_to :user, optional: true
+  has_many :line_items
+  mount_uploader :image, ImageUploader
   validates :brand, :model, :title, :price, presence: true
   validates :title, length: { maximum: 140, too_long: "%{count} characters is the maximum allowed."}
   validates :description, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed."}
@@ -9,4 +11,14 @@ class Item < ApplicationRecord
   BRAND = %w{ Nike Adidas Reebook Fila Other }
   FINISH = %w{ Black White Blue Red Yellow Other }
   CONDITION = %w{ New Excellent Mint Used Fair Poor }
+
+  private
+
+  def not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, "Line items present")
+      throw :abort
+    end
+    
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_25_043253) do
+ActiveRecord::Schema.define(version: 2020_10_25_235333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "brand"
@@ -30,6 +35,16 @@ ActiveRecord::Schema.define(version: 2020_10_25_043253) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
+  create_table "line_items", force: :cascade do |t|
+    t.bigint "item_id"
+    t.bigint "cart_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "quantity", default: 1
+    t.index ["cart_id"], name: "index_line_items_on_cart_id"
+    t.index ["item_id"], name: "index_line_items_on_item_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -44,4 +59,6 @@ ActiveRecord::Schema.define(version: 2020_10_25_043253) do
   end
 
   add_foreign_key "items", "users"
+  add_foreign_key "line_items", "carts"
+  add_foreign_key "line_items", "items"
 end
